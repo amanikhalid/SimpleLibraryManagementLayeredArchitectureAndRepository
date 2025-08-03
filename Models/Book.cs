cf9b2141cb3e26758cce9cbe3811ff63414d6af5
Amani Khalid <amanikhalidd@gmail.com>
2025-08-03 14:47:30 +0400

    Added Fields for the Book class

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,5 +8,11 @@ namespace SimpleLibraryManagementLayeredArchitectureAndRepository.Models
 {
     class Book
     {
+        // Fields for the Book class
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+
     }
 }
