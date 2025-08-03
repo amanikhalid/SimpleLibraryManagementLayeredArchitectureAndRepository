4bfb421e216cb74bea824ea9320c99b72c06cd8e
Amani Khalid <amanikhalidd@gmail.com>
2025-08-03 14:51:36 +0400

    Added Properties for the BorrowRecord class

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -8,5 +8,11 @@ namespace SimpleLibraryManagementLayeredArchitectureAndRepository.Models
 {
     class BorrowRecord
     {
+        // Properties for the BorrowRecord class
+        public int Id { get; set; }
+        public string MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; } // Nullable to allow for books that haven't been returned yet
     }
 }
