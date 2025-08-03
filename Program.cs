46a7617b2ec122960b590be4a677d234ec3bc2ad
Amani Khalid <amanikhalidd@gmail.com>
2025-08-03 14:24:32 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleLibraryManagementLayeredArchitectureAndRepository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
